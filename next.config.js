354ac11ba7e763a262a40eccff39b20f1d5c42da
Vidyadhar Purnavidyadharnani <purnavidyadharnani@gmail.com>
2026-03-23 11:39:19 +0530

    Fix next/image domain configuration error

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org', 'images.unsplash.com']
+    domains: ['res.cloudinary.com', 'firebasestorage.googleapis.com', 'img.icons8.com', 'raw.githubusercontent.com', 'i.imgur.com', 'img.freepik.com','media.geeksforgeeks.org', 'images.unsplash.com', 'iconlogovector.com', 'www.kindpng.com', 'seeklogo.com']
   }
 }
 
